b8e87bc3e0772972edcf4021782e437e50a63676
TalhaDereyurt <talhadereyurt52@gmail.com>
2023-05-20 15:37:13 +0300

    first commit

---
+++ talhaMahmut_dereyurt_1921221040_networklab_2023_proje/src/server/Message.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+/**
+ *
+ * @author talha
+ */
+public class Message implements java.io.Serializable {
+
+    //mesaj tipleri enum 
+    public static enum Message_Type {
+        None, Name, Disconnect, RivalConnected,Color,Turn, Text, Selected, Bitis, Start,
+    }
+    //mesajın tipi
+    public Message_Type type;
+    //mesajın içeriği obje tipinde ki istenilen tip içerik yüklenebilsin
+    public Object content;
+
+    public Message(Message_Type t) {
+        this.type = t;
+    }
+}
---
+++ talhaMahmut_dereyurt_1921221040_networklab_2023_proje/src/server/StartServer.java
@@ -0,0 +1,15 @@
+
+package server;
+
+/**
+ *
+ * @author talha
+ */
+public class StartServer {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Server.Start(2000);
+    }
+}
